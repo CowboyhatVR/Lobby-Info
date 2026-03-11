b9f05ed2b76ae3f1f37a0a02e8f8b4cc84840380
CowboyhatVR <maurits.gooijers@gmail.com>
2026-03-11 18:22:50 +0100

    Delete UnityEngineShim directory

--- UnityEngineShim/Forwarders.cs
+++
@@ -1,9 +0,0 @@
-using System.Runtime.CompilerServices;
-using UnityEngine;
-
-// Deze assembly heet "UnityEngine.dll" (via AssemblyName in csproj)
-// en forwardt de types naar de echte Unity 6 modules.
-[assembly: TypeForwardedTo(typeof(UnityEngine.MonoBehaviour))]
-[assembly: TypeForwardedTo(typeof(UnityEngine.Rect))]
-[assembly: TypeForwardedTo(typeof(UnityEngine.Screen))]
-[assembly: TypeForwardedTo(typeof(UnityEngine.GUI))]
